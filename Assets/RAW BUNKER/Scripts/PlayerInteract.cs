35299fcbc9c71255e74f27e048e90e41082827a0
Diego Alfonso <124203169+diegoalf1710@users.noreply.github.com>
2024-04-16 11:24:54 -0600

    Outline

---
+++ Assets/RAW BUNKER/Scripts/PlayerInteract.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public float playerReach = 3f;
+    interact currentinteract;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Checkinteract();
+        if(Input.GetKeyDown(KeyCode.F) && currentinteract != null)
+        {
+            currentinteract.Interact();
+        }
+    }
+
+    void Checkinteract()
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+        if(Physics.Raycast(ray, out hit, playerReach))
+        {
+            if(hit.collider.tag == "interactuable")
+            {
+                interact newInteract = hit.collider.GetComponent<interact>();
+
+                if(newInteract.enabled)
+                {
+                    SetNewCurrentinteract(newInteract);
+                }
+                else
+                {
+                    DisableCurrentinteract();
+                }
+            }
+            else
+            {
+                DisableCurrentinteract();
+            }
+        }
+    }
+
+    void SetNewCurrentinteract(interact newInteract)
+    {
+        currentinteract = newInteract;
+        currentinteract.EnableOutline();
+    }
+
+    void DisableCurrentinteract()
+    {
+        if (currentinteract)
+        {
+            currentinteract.DisableOutline();
+            currentinteract = null;
+        }
+    }
+}
