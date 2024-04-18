9763f5ee9e4d6ae77a1d868c3996794da7bb8887
Diego Alfonso <124203169+diegoalf1710@users.noreply.github.com>
2024-04-18 10:09:39 -0600

    avance drag de mouse

---
+++ Assets/RAW BUNKER/Scripts/MouseRotation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRotation : MonoBehaviour
+{
+    public float RotationSpeed = 1.0f;
+
+    void OnMouseDrag()
+    {
+        float XaxisRotation = Input.GetAxis("Mouse X") * RotationSpeed;
+
+        transform.Rotate(Vector3.down, XaxisRotation);
+    }
+}
